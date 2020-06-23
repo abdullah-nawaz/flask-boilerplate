1c847f23d1b6dfcf3e4c4079055f5c563f2e6699
Abdullah <muhammadabdullah@wanclouds.net>
2020-06-23 15:30:22 +0500

    user model added and migration created

--- run.py
+++ run.py
@@ -2,10 +2,11 @@
 import os
 
 from src.flask_boilerplate import create_app, db
+from src.flask_boilerplate.models.user import User
 
 app = create_app(os.getenv("FLASK_ENV", "development"))
 
 
 @app.shell_context_processor
 def shell():
-    return {"db": db}
+    return {"db": db, "User": User}
