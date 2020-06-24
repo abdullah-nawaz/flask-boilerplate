cbed1a4bbe1545508676a3e779008010902d6cdb
Abdullah <muhammadabdullah@wanclouds.net>
2020-06-24 23:06:07 +0500

    added auth name space to app->init

--- src/flask_boilerplate/api/__init__.py
+++ src/flask_boilerplate/api/__init__.py
@@ -2,6 +2,8 @@
 from flask import Blueprint
 from flask_restx import Api
 
+from src.flask_boilerplate.api.auth.endpoints import auth_ns
+
 api_bp = Blueprint("api", __name__, url_prefix="/api/v1")
 authorizations = {"Bearer": {"type": "apiKey", "in": "header", "name": "Authorization"}}
 
@@ -13,3 +15,6 @@ api = Api(
     doc="/ui",
     authorizations=authorizations,
 )
+
+
+api.add_namespace(auth_ns, path="/auth")
